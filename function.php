bc08a8fdf0ddf584c95089917516883b46e43171
TanvirHasan19 <54507026+TanvirHasan19@users.noreply.github.com>
2022-07-04 17:31:14 +0600

    Create function.php

---
+++ function.php
@@ -0,0 +1,8 @@
+// #-- Remove Product Types --#
+function remove_product_types( $product_types ){
+    unset( $product_types['variable'] );
+	unset( $product_types['external'] );
+	unset( $product_types['grouped'] );
+    return $product_types;
+}
+add_filter( 'dokan_product_types', 'remove_product_types', 11 );
